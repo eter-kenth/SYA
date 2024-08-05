1436b8e3dc010e8d6c27d024d4bf58ed1de655d3
eter-kenth <eter.kenth@gmail.com>
2024-08-05 15:58:07 -0600

    commit inicial

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	theme: {
+		extend: {
+		  boxShadow: {
+			'custom': '0px 0px 20px rgba(255, 255, 255)',
+		  },
+		  borderWidth: {
+			'3': '3px'// Añade el grosor de borde de 4px
+		  },
+		}
+	  },
+	  variants: {
+		extend: {},
+	  },
+	  plugins: [],
+
+}
